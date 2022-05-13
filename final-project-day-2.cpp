f1addbf3588418a98b098b1eca0af6851be177b8
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-05-13 16:35:15 -0400

    Update final-project-day-2.cpp

--- final-project-day-2.cpp
+++ final-project-day-2.cpp
@@ -16,4 +16,22 @@ int main() {
 	set_sprite_tile(0, 0);
 	move_sprite(0, 88, 78);
 	SHOW_SPRITES;
+
+while(1){
+	switch(joypad()){
+		case J_LEFT:
+			scroll_sprite(0,-10,0);
+			break;
+		case J_RIGHT:
+			scroll_sprite(0,10,0);
+			break;
+		case J_UP:
+			scroll_sprite(0,0,-10);
+			break;
+		case J_DOWN:
+			scroll_sprite(0,0,10);
+			break;
+		}
+		delay(1000);
+	}
 }
